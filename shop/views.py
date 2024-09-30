2b7714a0e4731e449d44df3526e71efe0bfd32ea
roman <wincentwangog@gmail.com>
2024-09-30 17:09:29 +0300

    templates

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course
+
 
 # Create your views here.
 def index(request): # имя index - потому что эта функция отвечает за страницу "/" в приложении shop
-    return HttpResponse("<main><h1>Hello from the Shop app</h1><p>This is my first app</p></main>")
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
+    # return HttpResponse("".join([f"{course}</br>" for course in courses]))
